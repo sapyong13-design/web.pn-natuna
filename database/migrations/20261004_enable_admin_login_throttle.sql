16bd2e734c1c7b8864845e55eeb7b6be2e976ee4
Faris <sapyong13@gmail.com>
2026-08-06 00:00:33 +0700

    fix: match production extension schema

--- database/migrations/20261004_enable_admin_login_throttle.sql
+++ database/migrations/20261004_enable_admin_login_throttle.sql
@@ -1,8 +1,9 @@
 INSERT INTO #__extensions
-(package_id,name,type,element,folder,client_id,enabled,access,protected,manifest_cache,params,custom_data,system_data,checked_out,checked_out_time,ordering,state)
-SELECT 0,'plg_system_loginthrottle','plugin','loginthrottle','system',0,1,1,0,
-'{"name":"plg_system_loginthrottle","type":"plugin","creationDate":"2026-08","author":"Pengadilan Negeri Natuna","copyright":"","authorEmail":"","authorUrl":"","version":"1.0.0","description":"Rate limits repeated Joomla administrator login failures.","group":"system","filename":"loginthrottle"}',
-'{}','','',0,NULL,0,0
+(package_id,name,type,element,folder,client_id,enabled,access,protected,locked,manifest_cache,params,custom_data,checked_out,checked_out_time,ordering,state)
+SELECT 0,'plg_system_loginthrottle','plugin','loginthrottle','system',0,1,1,0,0,
+'{"name":"plg_system_loginthrottle","type":"plugin","creationDate":"2026-08","author":"Pengadilan Negeri Natuna","version":"1.0.0","description":"Rate limits repeated Joomla administrator login failures.","group":"system","filename":"loginthrottle"}',
+'{}','',NULL,NULL,0,0
+FROM DUAL
 WHERE NOT EXISTS (SELECT 1 FROM #__extensions WHERE type='plugin' AND folder='system' AND element='loginthrottle');
 
 UPDATE #__extensions SET enabled=1
